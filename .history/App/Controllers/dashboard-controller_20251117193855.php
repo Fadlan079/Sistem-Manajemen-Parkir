81b8636fbae5628437096e00a600b30caf07b418
Fadlan079 <fadlanfirdaus220@gmail.com>
2025-12-24 16:14:26 +0800

    restructure project files

--- .history/App/Controllers/dashboard-controller_20251117193855.php
+++
@@ -1,5 +0,0 @@
-<?php 
-require_once __DIR__ . "/../../Resources/Views/index.php";
-
-
-?>
--- .history/App/Controllers/dashboard-controller_20251117194120.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once __DIR__ . "/../Models/";
-
-class DASHBOARDController{
-    private $pdo;
-
-    public function 
-}
-?>
